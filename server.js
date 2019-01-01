7756de5b87f6b65f5ca13a9cf5e70cb0977ab85a
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-01 20:18:51 +0530

    hbs added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const hbs = require('hbs');
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>{
