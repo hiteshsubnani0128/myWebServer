ff8df4cc5130d13edf487bfafe299a3ddd4d8488
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-01 22:30:05 +0530

    Setup done

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -44,6 +46,6 @@ app.get('/about', (req, res) =>{
 	});
 });
 
-app.listen(3000, () =>{
-	console.log('Server is up at Port 3000');
+app.listen(port, () =>{
+	console.log(`Server is up at Port ${port}`);
 });
