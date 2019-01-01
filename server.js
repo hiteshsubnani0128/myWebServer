e5a49d90089932df5e3404952962f7f2f67afcb2
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-01 21:53:40 +0530

    log saved

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
-
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
-
 hbs.registerHelper('getCurrentYear', () =>{
 	return new Date().getFullYear()
 });
 
 hbs.registerHelper('screamIt', (text) =>{
 	return text.toUpperCase();
+});
 
-})
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) =>{
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFileSync('server.log',log + '\n');
+	next();
+});
 
 app.get('/', (req, res) =>{
 	res.render('home.hbs',{
