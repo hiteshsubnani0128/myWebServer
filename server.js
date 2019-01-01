2064b0fcd9887eed5fb625763bf96ee46a21306b
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-01 21:40:15 +0530

    hbs added as helper function

--- server.js
+++ server.js
@@ -4,21 +4,29 @@ const hbs = require('hbs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () =>{
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+	return text.toUpperCase();
+
+})
 
 app.get('/', (req, res) =>{
 	res.render('home.hbs',{
 		pageTitle : 'HomePage',
-		currentYear : new Date().getFullYear(),
 		welcomeMessage : 'Welcome to my website'
 	});
 });
 
 app.get('/about', (req, res) =>{
 	res.render('about.hbs', {
-		pageTitle : 'About Page',
-		currentYear : new Date().getFullYear()
+		pageTitle : 'About Page'
 	});
 });
 
